101d6f6434983d2d28a83b4fa6aa5d54d17b2f3b
Solodovnikova Ekaterina <colo18@yandex.ru>
2020-03-18 19:23:08 +0500

    added Router for App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import  Survey  from './components/Survey/Survey';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Survey />
+    <Router>
+      <Switch>
+         <Route path="/" exact>
+            <Survey />
+          </Route>
+          <Route path="/login">
+            {/* <LoginPanel /> */}
+          </Route>
+          <Route path="/admin">
+            {/* <AdminPanel /> */}
+          </Route>
+      </Switch>
+    </Router>
     </div>
   );
 }
