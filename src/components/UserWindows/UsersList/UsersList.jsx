ec8b34c9725df7f2cfcb66c6db5e047963d97f23
Solodovnikova Ekaterina <colo18@yandex.ru>
2020-04-06 20:53:21 +0500

    added users list design

--- src/components/UserWindows/UsersList/UsersList.jsx
+++ src/components/UserWindows/UsersList/UsersList.jsx
@@ -26,21 +26,24 @@ export class UsersList extends Component {
 	render(){
 		return (
 			<div className='usersList'>
-				<div className='totalNumber'><b>Всего пользователей:</b>
-					{this.state.dataIsFetched ? this.state.totalNumber : <Loader color='black' type="Bars" width={20} height={20}/>}
+				<div className='formTitle'>Список пользователей</div>
+				<div className='totalNumber'>
+					<b>Всего пользователей:</b>
+					<span className='totalCounter'>{ this.state.totalNumber }</span>
+					{/*{this.state.dataIsFetched ? this.state.totalNumber : <Loader color='black' type="Bars" width={20} height={20}/>}*/}
 				</div>
-				{!this.state.dataIsFetched && (
-					<Loader type="Bars" color="black"/>
-				)}
-				{this.state.dataIsFetched && (
-					<ol>
-						{this.state.users.map((user, key) => {
-							return (
-								<li key={key}>{user.name}</li>
-							);
-						})}
-					</ol>
-				)}
+				{/*{!this.state.dataIsFetched && (*/}
+				{/*	<Loader type="Bars" color="black"/>*/}
+				{/*)}*/}
+				{/*{this.state.dataIsFetched && (*/}
+				<ol>
+					{this.state.users.map((user, key) => {
+						return (
+							<li key={key}>{user.name}</li>
+						);
+					})}
+				</ol>
+				{/*)}*/}
 			</div>
 		);
 	}
