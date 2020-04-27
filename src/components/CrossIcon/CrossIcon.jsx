f2f3112b1a77c71133630132a6df8f2f14980026
Yuliya Belorussova <belorussova@MacBook-Pro-Belorussova.local>
2020-04-27 14:57:26 +0500

    Убрала ещё лишнего

--- src/components/CrossIcon/CrossIcon.jsx
+++ src/components/CrossIcon/CrossIcon.jsx
@@ -1,29 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactTooltip from 'react-tooltip';
 import './CrossIcon.css';
 
-export default class CrossIcon extends Component {
-	renderTooltip() {
-		return(
-			<div className='delete-tooltip'>
-				{this.props.question}
-			</div>
-		);
-	}
-
-	render() {
-		return (
-			<>
-				<div data-tip={this.props.question} onClick={this.props.onClick} className="cross-icon">
+export default function CrossIcon(props) {
+	return (
+		<>
+			<div data-tip={props.question} onClick={props.onClick} className="cross-icon">
                     ✕
-				</div>
-				<ReactTooltip 
-					className='delete-tooltip' 
-					backgroundColor='#c4c4c4' 
-					textColor='#000000'
-					effect='solid' 
-				/>
-			</>
-		);
-	}
+			</div>
+			<ReactTooltip 
+				className='delete-tooltip' 
+				backgroundColor='#c4c4c4' 
+				textColor='#000000'
+				effect='solid' 
+			/>
+		</>
+	);
 }
