160181d37eccfd1b398e6ab8866f79ce48498c19
Solodovnikova Ekaterina <colo18@yandex.ru>
2020-04-29 17:44:19 +0500

    style fixes for new code

--- src/api/result.js
+++ src/api/result.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 export function postResult({ sessionId, form, content, interest, name, comment }) {
-    return axios.post(process.env.REACT_APP_URL + '/results', {
-        sessionId,
-        form,
-        content,
-        interest,
-        username: name,
-        comment
-    });
+	return axios.post(process.env.REACT_APP_URL + '/results', {
+		sessionId,
+		form,
+		content,
+		interest,
+		username: name,
+		comment
+	});
 }
 
 export function getResult(id) {
-    return axios.get(`${ process.env.REACT_APP_URL }/results?id=${id}`);
+	return axios.get(`${ process.env.REACT_APP_URL }/results?id=${id}`);
 }
